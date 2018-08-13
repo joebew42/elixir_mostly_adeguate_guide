a6c8b5987d01d4b195938b681a13e54711c82c0f
JoeBew42 <joebew42@gmail.com>
2018-08-13 11:37:15 +0200

    add curry function

--- test/chapter4_currying_test.exs
+++ test/chapter4_currying_test.exs
@@ -14,4 +14,25 @@ defmodule Chapter4CurryingTest do
     assert increment.(2) == 3
     assert add_ten.(2) == 12
   end
+
+  test "curry" do
+    curried = curry(fn a, b -> a + b end)
+
+    assert curried.(1).(1) == 2
+  end
+
+  # http://blog.patrikstorm.com/function-currying-in-elixir
+  defp curry(function) do
+    {_, arity} = :erlang.fun_info(function, :arity)
+    curry(function, arity, [])
+  end
+
+  defp curry(function, 0, arguments), do:
+    apply(function, Enum.reverse(arguments))
+  end
+  defp curry(function, arity, arguments) do
+    fn argument ->
+      curry(function, arity - 1, [argument | arguments])
+    end
+  end
 end
