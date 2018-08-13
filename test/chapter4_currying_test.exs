d528ba9168157460321dc303a5dca846d4fd2e1d
JoeBew42 <joebew42@gmail.com>
2018-08-13 11:42:12 +0200

    format code

--- test/chapter4_currying_test.exs
+++ test/chapter4_currying_test.exs
@@ -30,6 +30,7 @@ defmodule Chapter4CurryingTest do
   defp curry(function, 0, arguments) do
     apply(function, Enum.reverse(arguments))
   end
+
   defp curry(function, arity, arguments) do
     fn argument ->
       curry(function, arity - 1, [argument | arguments])
