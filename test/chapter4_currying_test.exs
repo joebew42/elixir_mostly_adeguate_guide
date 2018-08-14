931be91a412a8a4f5a38836b97015c8295c3091c
JoeBew42 <joebew42@gmail.com>
2018-08-14 11:10:11 +0200

    complete the example

--- test/chapter4_currying_test.exs
+++ test/chapter4_currying_test.exs
@@ -21,8 +21,10 @@ defmodule Chapter4CurryingTest do
     assert curried.(3).(1) == 2
   end
 
-  test "match" do
+  test "match, replace, filter and map" do
     match = curry(fn what, string -> Regex.match?(what, string) end)
+    filter = curry(fn function, enum -> Enum.filter(enum, function) end)
+    replace = curry(fn what, replacement, string -> String.replace(string, what, replacement) end)
 
     assert match.(~r/r/).("hello world")
 
@@ -31,9 +33,16 @@ defmodule Chapter4CurryingTest do
     assert has_letter_r?.("hello world")
     refute has_letter_r?.("just j and s and t etc")
 
-    names = ["joe", "foo", "bar"]
+    assert filter.(has_letter_r?).(["rock and roll", "smooth jazz"]) == ["rock and roll"]
 
-    assert Enum.filter(names, has_letter_r?) == ["bar"]
+    remove_strings_without_rs = filter.(has_letter_r?)
+
+    assert remove_strings_without_rs.(["rock and roll", "smooth jazz"]) == ["rock and roll"]
+
+    no_vowels = replace.(~r/[aeiou]/)
+    censored = no_vowels.("*")
+
+    assert censored.("Chocolate Rain") == "Ch*c*l*t* R**n"
   end
 
   # http://blog.patrikstorm.com/function-currying-in-elixir
