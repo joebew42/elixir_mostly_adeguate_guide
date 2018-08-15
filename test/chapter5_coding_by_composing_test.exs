9fac78ebf7c9f90dc843086befd8251735b5ae7d
JoeBew42 <joebew42@gmail.com>
2018-08-15 20:19:44 +0200

    first example of chapter 5

--- test/chapter5_coding_by_composing_test.exs
+++ test/chapter5_coding_by_composing_test.exs
@@ -1,7 +1,17 @@
 defmodule Chapter5CodingByComposingTest do
   use ExUnit.Case, async: true
 
-  test "xxx" do
-    assert true
+  test "Functional Husbandry" do
+    to_upper_case = fn string -> String.upcase(string) end
+    exclaim = fn string -> "#{string}!" end
+    shout = compose(exclaim, to_upper_case)
+
+    assert shout.("send in the clowns") == "SEND IN THE CLOWNS!"
+  end
+
+  def compose(f, g) do
+    fn x ->
+      f.(g.(x))
+    end
   end
 end
